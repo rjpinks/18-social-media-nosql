95f693145b1df8a66ea281bdf7d5c62b8ebdae39
Pinky <pinkstonr54@gmail.com>
2023-05-30 18:06:49 -0500

    routes and controllers written for user

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require("./routes")
 
-const { /* model files */ } = require('./models');
+//const { User, Thought } = require('./models');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//app.use(routes);
+app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
